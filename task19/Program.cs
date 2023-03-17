964925d160e16b32b80789eb1125d6d94c200550
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-03-17 22:37:00 +0700

    added task19

---
+++ task19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+// Напишите программу, 
+//которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int A = int.Parse(Console.ReadLine());
+string num = Convert.ToString(A);
+if ((num[0] == num[4]) && (num[1] == num[3]))
+    Console.WriteLine($"{num} -> yes");
+else
+    Console.WriteLine($"{num} -> no");
